99487e836e2929a2097dbb5ebeff585693b603ab
向浩 <921223344@qq.com>
2025-12-13 00:43:59 +0800

    Initial implementation of the project structure and basic functionality.

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+/* eslint-env node */
+require('@vue/eslint-config-prettier')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
+
